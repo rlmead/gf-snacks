4ba200704143fe5747d4c5f84b66f9efb4cd1622
Rebecca Mead <beckyleemead@gmail.com>
2021-04-22 12:46:28 -0400

    add credits to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,37 @@
-import React from "react";
-import { Navbar, Nav } from "reactstrap";
+import React, { useState } from "react";
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem } from "reactstrap";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  let navItems =
+    [
+      { to: "https://www.savoringkentucky.com", text: "Inspired by ", linkText: "Rona" },
+      { to: "https://also-and.web.app", text: "Built by ", linkText: "Becky" },
+      { to: "https://unsplash.com/@brookelark", text: "Photo by ", linkText: "Brooke" }
+    ]
+
   return (
     <Navbar
-      className="sticky-top bg-primary text-light shadow">
-      <Nav>
-        <h1>Gluten-free snack recipe generator</h1>
-      </Nav>
+      className="sticky-top bg-primary text-light shadow navbar-dark">
+      <h1>Gluten-free snack recipe generator</h1>
+      <NavbarToggler onClick={toggle} />
+      <Collapse
+        isOpen={isOpen}
+        navbar >
+        <Nav style={{ display: "flex", flexFlow: "row nowrap" }} navbar>
+          {
+            navItems.map((item, index) => {
+              return (
+                <NavItem className="mx-auto">
+                  <a key={index} className="nav-link text-light" target="_blank" href={item.to}>{item.text}<u>{item.linkText}</u></a>
+                </NavItem>
+              )
+            })
+          }
+        </Nav>
+      </Collapse>
     </Navbar>
 
   );
